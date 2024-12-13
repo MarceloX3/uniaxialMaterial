cbcd2c548a503b3f1d789b4074c461bcac2f49fe
Marcelo Alejandro Ortiz Avila <124118183+MarceloX3@users.noreply.github.com>
2024-12-13 03:05:28 -0300

    Update S01_GUI02_A04_2_testUniaxialMaterial.py

--- C_GUI02_uniaxialMaterial/S01_GUI02_A04_2_testUniaxialMaterial.py
+++ C_GUI02_uniaxialMaterial/S01_GUI02_A04_2_testUniaxialMaterial.py
@@ -60,16 +60,29 @@ elif model == 'Steel01':
     ops.uniaxialMaterial('Steel01', MatTag, Fy, E0, b, a1, a2, a3, a4)
 
 elif model == 'Steel4':
-    Fy, E0, b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, f_u, R_u, f_uc, R_uc, sig_init, cycNum = model_args_x[2:]
-    Fy, E0, b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, f_u, R_u, f_uc, R_uc, sig_init, cycNum = float(
-        Fy), float(E0), float(b_k), float(R0), float(r1), float(r2), float(b_kc), float(R0c), float(r1c), float(r2c), float(b_i), float(rho_i), float(b_I), float(R_i), float(I_yp), float(b_ic), float(rho_ic), float(b_Ic), float(R_ic), float(f_u), float(R_u), float(f_uc), float(R_uc), float(sig_init), int(cycNum)
+    flag, Fy, E0, b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, f_u, R_u, f_uc, R_uc, sig_init, cycNum = model_args_x[2:]
+    flag, Fy, E0, b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, f_u, R_u, f_uc, R_uc, sig_init, cycNum = str(
+        flag), float(Fy), float(E0), float(b_k), float(R0), float(r1), float(r2), float(b_kc), float(R0c), float(r1c), float(r2c), float(b_i), float(rho_i), float(b_I), float(R_i), float(I_yp), float(b_ic), float(rho_ic), float(b_Ic), float(R_ic), float(f_u), float(R_u), float(f_uc), float(R_uc), float(sig_init), int(cycNum)
     params = [R0, r1, r2]
-    # ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-kin', b_k, *params, b_kc, R0c, r1c, r2c, '-iso', b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, '-ult', f_u, R_u, f_uc, R_uc, '-init', sig_init, '-mem', cycNum)
-    # ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-kin', b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, '-iso', b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, '-ult', f_u, R_u, f_uc, R_uc, '-init', sig_init, '-mem', cycNum)
-    ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-kin', b_k, R0, r1, r2, b_kc, R0c, r1c, r2c)
-    # ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-iso', b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic)
-    # ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-iso', b_i, rho_i, b_I, R_i, I_yp)
-    # ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-ult', f_u, R_u)
+    
+    #['-kin', '-iso', '-asym -kin', '-asym -iso', '-kin -ult', '-iso -ult', '-asym -kin -ult', '-asym -iso -ult']
+    if flag == '-kin':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-kin', b_k, R0, r1, r2, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-iso':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-iso', b_i, rho_i, b_I, R_i, I_yp, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-asym -kin':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-kin', b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-asym -iso':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-iso', b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-kin -ult':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-kin', b_k, R0, r1, r2, '-ult', f_u, R_u, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-iso -ult':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-iso', b_i, rho_i, b_I, R_i, I_yp, '-ult', f_u, R_u, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-asym -kin -ult':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-kin', b_k, R0, r1, r2, b_kc, R0c, r1c, r2c, '-ult', f_u, R_u, f_uc, R_uc, '-init', sig_init, '-mem', cycNum)
+    elif flag == '-asym -iso -ult':
+        ops.uniaxialMaterial('Steel4', MatTag, Fy, E0, '-asym', '-iso', b_i, rho_i, b_I, R_i, I_yp, b_ic, rho_ic, b_Ic, R_ic, '-ult', f_u, R_u, f_uc, R_uc, '-init', sig_init, '-mem', cycNum)
+    
     
 # If there ia a MinMax material
 if len(min_max_args_x) != 0:
